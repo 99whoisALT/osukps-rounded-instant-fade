8e517ea41460da4261a52641effdbc8e0cf92daf
99 <76541007+99whoisALT@users.noreply.github.com>
2025-09-28 22:28:06 -0400

    Update KpsButton.cs

--- osukps/KpsButton.cs
+++ osukps/KpsButton.cs
@@ -126,6 +126,7 @@ namespace osukps {
 			int g = color.inactive.G + (int) (f * (color.active.G - color.inactive.G));
 			int b = color.inactive.B + (int) (f * (color.active.B - color.inactive.B));
 			currentcol = Color.FromArgb(255, r, g, b);
+			label.BackColor = Color.FromArgb(255, r, g, b);
 			label.Invalidate(false);
 		}
 
